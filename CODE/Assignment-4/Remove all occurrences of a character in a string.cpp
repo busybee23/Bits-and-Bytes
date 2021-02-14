65f533b0a06be6013261228edbe606e9c539262b
Yash Chaudhary <chaudharyyash088@gmail.com>
2021-02-14 22:44:08 +0530

    added last class questions

---
+++ CODE/Assignment-4/Remove all occurrences of a character in a string.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+string removeCharRecursive(string str, char X) { 
+	if (str.length() == 0) return ""; 		
+	if (str[0] == X) return removeCharRecursive(str.substr(1), X); 	
+	return str[0] + removeCharRecursive(str.substr(1), X); 
+} 
+
+int main() 
+{ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string str; cin>>str;
+	char X; cin>>X;
+ 
+	str = removeCharRecursive(str, X); 
+	cout << str; 
+	return 0; 
+}
