28f6e773a2ff24c4426818b3de1e0a1a819e770e
Yash Chaudhary <chaudharyyash088@gmail.com>
2021-03-29 13:05:57 +0530

    Added Power Function Using Recursion

---
+++ NOTES/Power Function Using Recursion.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int power(int num, int x){
+    // base case 
+    if(x==0){
+        return 1;
+    }
+     // // recursive case 
+    // int SmallerProblem = power(num,x-1;)
+    // int BiggerProblem = num*(power(num,x-1))
+    
+    return num*(power(num,x-1));
+}
+
+int main (){
+    int num,x;cin>>num>>x;
+    cout<<power(num,x)<<endl;
+}
